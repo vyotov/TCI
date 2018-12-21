6db8e807ae189f0991d4d3c9c7d80ce6e7fa80f7
Todor Kosev <>
2018-12-21 22:50:52 +0100

    fixed tupos

--- src/test/java/com/company/Models/MusicTest.java
+++ src/test/java/com/company/Models/MusicTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertNotEquals;
 public class MusicTest{
 
     @Test
-    public void IsGettingRgihtName() {
+    public void IsGettingRightName() {
         //TODO
     }
     @Test
