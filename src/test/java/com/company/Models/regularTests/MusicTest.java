815a5cc868260f7f04278dafcbb41b0b46c96c93
Ventislav Yotov <vyotov@Ventsislavs-MacBook-Pro.local>
2019-01-12 19:01:53 +0100

    Music Regular Tests

--- src/test/java/com/company/Models/regularTests/MusicTest.java
+++ src/test/java/com/company/Models/regularTests/MusicTest.java
@@ -1,4 +1,6 @@
 package com.company.Models.regularTests;
+import com.company.Models.Movie;
+import com.company.Models.Music;
 import org.junit.Before;
 import junitparams.Parameters;
 import org.junit.Assert;
@@ -11,26 +13,68 @@ public class MusicTest{
 
     @Test
     public void IsGettingRightName() {
-        //TODO
+        //Arrange
+        Music music = new Music();
+        music.setName("Elvis Forever");
+        String actual = "Elvis Forever";
+        //Act
+        String expected = music.getName();
+        //Assert
+        Assert.assertEquals(expected,actual);
     }
     @Test
     public void IsGettingRgihtCategory() {
-        //TODO
+        //Arrange
+        Music music = new Music();
+        music.setCategory("Music");
+        String actual = "Music";
+        //Act
+        String expected = music.getCategory();
+        //Assert
+        Assert.assertEquals(expected,actual);
     }
     @Test
     public void IsGettingRightGenre() {
-        //TODO
+        //Arrange
+        Music music = new Music();
+        music.setGenre("Rock");
+        String actual = "Rock";
+        //Act
+        String expected = music.getGenre();
+        //Assert
+        Assert.assertEquals(expected,actual);
     }
     @Test
     public void IsGettingRightFormat() {
-        //TODO
+        //Arrange
+        Music music = new Music();
+        music.setFormat("Vinyl");
+        String actual = "Vinyl";
+        //Act
+        String expected = music.getFormat();
+        //Assert
+        Assert.assertEquals(expected,actual);
     }
     @Test
     public void IsGettingRightYear() {
-        //TODO
+        //Arrange
+        Music music = new Music();
+        music.setYear("2015");
+        String actual = "2015";
+        //Act
+        String expected = music.getYear();
+        //Assert
+        Assert.assertEquals(expected,actual);
     }
     @Test
     public void IsGettingRightArtist() {
-        //TODO
+        //Arrange
+        Music music = new Music();
+        music.setArtist("Elvis Presley");
+        String actual = "Elvis Presley";
+        //Act
+        String expected = music.getArtist();
+        //Assert
+        Assert.assertEquals(expected,actual);
     }
 }
