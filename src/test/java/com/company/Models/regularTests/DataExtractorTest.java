f53e5384f4b04bc0cc387bc431231a41a29aace9
Todor Kosev <>
2019-01-12 17:26:52 +0100

    created test class for java object ExtractorTest and DataExtractorTest

---
+++ src/test/java/com/company/Models/regularTests/DataExtractorTest.java
@@ -0,0 +1,25 @@
+package com.company.Models.regularTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataExtractorTest {
+
+    @Test
+    public void setUrl() {
+
+    }
+
+    @Test
+    public void parseMusic() {
+    }
+
+    @Test
+    public void parseMovie() {
+    }
+
+    @Test
+    public void parseBook() {
+    }
+}
---
+++ src/test/java/com/company/SearchAlgorithms/ExtractorTest.java
@@ -0,0 +1,28 @@
+package com.company.SearchAlgorithms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExtractorTest {
+
+    @Test
+    public void getPageLinks() {
+    }
+
+    @Test
+    public void searchById() {
+    }
+
+    @Test
+    public void getAllObjects() {
+    }
+
+    @Test
+    public void getTimeDuration() {
+    }
+
+    @Test
+    public void getPageCount() {
+    }
+}
