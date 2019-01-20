4b2bcf42842e4c528c2b534b09003f236eb9fb1e
Todor Kosev <>
2019-01-20 15:41:13 +0100

    added testSearchByIdForMusicModel and testSearchByIdForMovieModel hamcrest instance checks test method

--- src/test/java/com/company/Models/matcher/HamcrestMatcherTest.java
+++ src/test/java/com/company/Models/matcher/HamcrestMatcherTest.java
@@ -1,34 +1,46 @@
 package com.company.Models.matcher;
 
 import com.company.Models.Book;
+import com.company.Models.Movie;
+import com.company.Models.Music;
 import com.company.Models.custom_rule.LoggerRule;
 import com.company.SearchAlgorithms.Extractor;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 
 public class HamcrestMatcherTest {
 
-
     @Rule
     public LoggerRule performanceLogger = new LoggerRule();
+
     private Extractor extractor;
 
     @Before
     public void setup() throws MalformedURLException {
         extractor = new Extractor("http://localhost:8888");
-
     }
 
     @Test
     public void testSearchByIdForBookModel() throws IOException, ClassNotFoundException {
         //Assert
         assertThat(extractor.searchById("102"), instanceOf(Book.class));
+    }
 
+    @Test
+    public void testSearchByIdForMusicModel() throws IOException, ClassNotFoundException {
+        //Assert
+        assertThat(extractor.searchById("302"), instanceOf(Music.class));
+    }
+
+    @Test
+    public void testSearchByIdForMovieModel() throws IOException, ClassNotFoundException {
+        //Assert
+        assertThat(extractor.searchById("202"), instanceOf(Movie.class));
     }
 }
