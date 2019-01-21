725a8b58a1534ebd90c4e639d3ecc2af309d49bb
Ventislav Yotov <vyotov@Ventsislavs-MacBook-Pro.local>
2019-01-21 21:21:03 +0100

    WIP

--- src/test/java/com/company/Models/mokitoTests/MokitoTest.java
+++ src/test/java/com/company/Models/mokitoTests/MokitoTest.java
@@ -5,6 +5,7 @@ import com.company.Models.Movie;
 import com.company.SearchAlgorithms.DataExtractor;
 import com.company.SearchAlgorithms.Extractor;
 import com.company.utils.Utils;
+import org.json.simple.JSONObject;
 import org.jsoup.nodes.Element;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,6 +16,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Arrays;
+import java.util.HashSet;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -29,37 +31,65 @@ public class MokitoTest {
     private Utils mockedUtils;
     @Mock
     private Element element;
+    @Mock
+    private HashSet<String> mockedLinks;
     //MOCKITO DUMMY OBJ
     @Test
     public void mockitoDummyObjectTest()throws IOException, ClassNotFoundException, IllegalAccessException {
-
-        String url = "http://localhost:8888";
-        //Creation of SUT
-        // mockedDataExtractor and mockedUtiles are injected into SUT
-        Extractor extractor = new Extractor(url,mockedDataExtractor,mockedUtils);
-        //Execution of the SUT’s method using a dummy mockedDataExtractor object as one of the parameters
-        extractor.searchById("201",mockedDataExtractor);
-        //mockedDataExtractor.parseBook(element);
+//        //Arrange
+//        String url = "http://localhost:8888";
+//        String detailPage = "http://localhost:8888/sample_site_to_crawl/details.php?id=202";
+//
+//        Movie movie = getMovie();
+//
+//        when(mockedUtils.findCategory(detailPage)).thenReturn(Category.MOVIE);
+//        when(mockedDataExtractor.parseMovie(element)).thenReturn(movie);
+//        when(mockedUtils.getElement()).thenReturn(element);
+//        //Creation of SUT
+//        // mockedDataExtractor and mockedUtiles are injected into SUT
+//        Extractor extractor = new Extractor(url,mockedDataExtractor,mockedUtils);
+//        //Execution of the SUT’s method using a dummy mockedDataExtractor object as one of the parameters
+//        extractor.searchById("202",mockedDataExtractor);
 
     }
+    // MOCKITO EXCEPTIONS
+    @Test(expected = ClassNotFoundException.class)
+    public void throwException() throws IllegalAccessException, IOException, ClassNotFoundException {
+        when(mockedExtractor.getAllObjects())
+                .thenThrow(new ClassNotFoundException());
+        mockedExtractor.getAllObjects();
+        JSONObject actual = mockedExtractor.getAllObjects();
+        String expected = "{\"name\":\"Elvis Forever\",\"category\":\"Music\",\"genre\":\"Rock\",\"format\":\"Vinyl\",\"year\":\"2015\",\"artist\":\"Elvis Presley\"}";
+        Assert.assertEquals(expected,actual.toString());
+    }
+
     //INDIRECT INPUT
     @Test
-    public void verifySearchById() throws IOException, ClassNotFoundException, IllegalAccessException {
-
+    public void verifyParseMovie() throws IOException, IllegalAccessException, ClassNotFoundException {
         String url = "http://localhost:8888";
         String detailPage = "http://localhost:8888/sample_site_to_crawl/details.php?id=202";
         //arrange
         Movie movie = getMovie();
 
-        when(mockedDataExtractor.getUrl()).thenReturn(detailPage);
+        when(mockedUtils.findCategory(detailPage)).thenReturn(Category.MOVIE);
+        when(mockedUtils.getElement()).thenReturn(element);
+        Extractor extractor = new Extractor(url,mockedDataExtractor,mockedUtils);
+
+        extractor.searchById("202",mockedDataExtractor);
+        verify(mockedDataExtractor).parseMovie(element);
+    }
+    @Test
+    public void testSearchById() throws IOException, ClassNotFoundException, IllegalAccessException {
+        String url = "http://localhost:8888";
+        String detailPage = "http://localhost:8888/sample_site_to_crawl/details.php?id=202";
+        //arrange
+        Movie movie = getMovie();
         when(mockedUtils.findCategory(detailPage)).thenReturn(Category.MOVIE);
         when(mockedDataExtractor.parseMovie(element)).thenReturn(movie);
         when(mockedUtils.getElement()).thenReturn(element);
         Extractor extractor = new Extractor(url, mockedDataExtractor,mockedUtils);
-
         Object obj = extractor.searchById("202", mockedDataExtractor);
         Assert.assertEquals(obj, movie);
-        //verify(mockedDataExtractor).getUrl();
     }
 
     @Test
@@ -70,6 +100,11 @@ public class MokitoTest {
         //behave
         when(mockedExtractor.getPageCount()).thenReturn(22);
         verify(mockedExtractor).getPageCount();
+
+    }
+    //INDIRECT OUTPUT
+    @Test
+    public void testPageLinks() throws MalformedURLException {
 
     }
 
